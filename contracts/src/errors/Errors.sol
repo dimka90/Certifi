a483d1edf78941e8e914cdf77d2dcd2b01e0695a
dimka90 <dimkayilrit@gmail.com>
2026-01-13 15:36:33 +0100

    fix(contracts): resolve error/event name collision and add missing errors

--- contracts/src/errors/Errors.sol
+++ contracts/src/errors/Errors.sol
@@ -1,68 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-
-error NotOwner();
-error NotAuthorizedInstitution();
-error NotIssuingInstitution();
-
-// Institution Errors
-error InstitutionAlreadyRegistered();
-error InstitutionNotRegistered();
-error InstitutionNotAuthorized();
-error InstitutionAlreadyAuthorized();
-error InvalidInstitutionAddress();
-error InvalidInstitutionID();
-
-// Certificate Errors
-error CertificateDoesNotExist();
-error CertificateAlreadyRevoked();
-error CertificateNotRevoked();
-error InvalidStudentAddress();
-error InvalidTokenURI();
-error InvalidCGPA();
-error CertificateExpired();
-error SoulboundTokenNoTransfer();
-error BatchSizeCheckFailed();
-error InvalidRevocationReason(); // Added
-
-// General Errors
-error InvalidAddress();
-error EmptyString();
-error ContractPaused(); // Added
-error InvalidIndex(); // Added
-
-// Template Errors
-error TemplateNotFound();
-error TemplateAlreadyExists();
-error TemplateValidationFailed();
-error InvalidTemplateData();
-error TemplateNotActive();
-
-// Multi-Signature Errors
-error OperationNotFound();
-error OperationAlreadyExecuted();
-error InsufficientSignatures();
-error AlreadySigned();
-error NotAuthorizedSigner();
-error InvalidThreshold();
-
-// Analytics Errors
-error InvalidTimeRange();
-error AnalyticsNotAvailable();
-error InsufficientData();
-
-// Verification Errors
-error InvalidVerificationCode();
-error VerificationExpired();
-error VerificationMethodNotSupported();
-
-// Role and Access Control Errors
-error RoleNotFound();
-error InsufficientPermissions();
-error RoleAlreadyAssigned();
-error InvalidRoleData();
-
-// Lifecycle Errors
-error CertificateNotRenewable();
-error AmendmentNotAllowed();
-error InvalidAmendmentData();
+error NotOwner(); error NotAuthorizedInstitution(); error NotIssuingInstitution();
+error InstitutionAlreadyRegistered(); error InstitutionNotRegistered(); error InstitutionNotAuthorized(); error InstitutionAlreadyAuthorized();
+error InvalidInstitutionAddress(); error InvalidInstitutionID();
+error CertificateDoesNotExist(); error CertificateAlreadyRevoked(); error CertificateNotRevoked();
+error InvalidStudentAddress(); error InvalidTokenURI(); error InvalidCGPA();
+error CertificateHasExpired(); error SoulboundTokenNoTransfer(); error BatchSizeCheckFailed();
+error InvalidRevocationReason(); error InvalidAddress(); error EmptyString(); error ContractPaused(); error InvalidIndex();
+error TemplateNotFound(); error TemplateAlreadyExists(); error TemplateValidationFailed(); error InvalidTemplateData(); error TemplateNotActive();
+error OperationNotFound(); error OperationAlreadyExecuted(); error InsufficientSignatures(); error AlreadySigned(); error NotAuthorizedSigner(); error InvalidThreshold();
+error InvalidTimeRange(); error AnalyticsNotAvailable(); error InsufficientData();
+error InvalidVerificationCode(); error VerificationExpired(); error VerificationMethodNotSupported();
+error RoleNotFound(); error InsufficientPermissions(); error RoleAlreadyAssigned(); error InvalidRoleData(); error AccessDenied(bytes32 role);
+error CertificateNotRenewable(); error AmendmentNotAllowed(); error InvalidAmendmentData();
+error NotClaimable(); error CertificateAlreadyClaimed(); error InvalidClaimCode();
