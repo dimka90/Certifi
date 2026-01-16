d0480926cd968bda67485f51ff7ffb8b21e7dc92
dimka90 <dimkayilrit@gmail.com>
2026-01-16 03:28:44 +0100

    test: [FIX] Correct initialization and role setup in VerificationRequestTest

--- contracts/test/VerificationRequest.t.sol
+++ contracts/test/VerificationRequest.t.sol
@@ -5,9 +5,13 @@ import "../src/core/CertificationNft.sol";
 contract VerificationRequestTest is Test {
     CertificateNFT nft; address admin = address(1); address inst = address(2);
     function setUp() public {
-        vm.prank(admin); nft = new CertificateNFT(); vm.prank(admin); nft.grantRole(nft.ADMIN_ROLE(), admin);
-        vm.prank(inst); nft.registerInstitution("Test Inst", "ID1", "email", "country", InstitutionType.University);
-        vm.prank(admin); nft.authorizeInstitution(inst);
+        nft = new CertificateNFT();
+        nft.grantRole(nft.ADMIN_ROLE(), admin);
+        
+        vm.prank(inst);
+        nft.registerInstitution("Test Inst", "ID1", "email", "country", InstitutionType.University);
+        
+        nft.authorizeInstitution(inst);
     }
     function testRequestVerification() public {
         CertificateData memory data = CertificateData({ studentWallet: address(3), studentName: "Student", studentID: "S1", degreeTitle: "Degree", grade: Classification.FirstClass, duration: "4 years", cgpa: "4.0", faculty: Faculty.Science, tokenURI: "uri", expirationDate: 0, templateId: 0, isClaimable: false, claimHash: bytes32(0) });
