f04698a4368caa274f465136220d4b2342b03af0
shaaibu7 <shaaibusuleiman9@gmail.com>
2026-01-16 12:49:57 +0100

    feat: add pausable functionality for emergency stops

--- contracts/src/core/SimpleToken.sol
+++ contracts/src/core/SimpleToken.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
 /**
  * @title SimpleToken
  * @dev Basic ERC20 token implementation with ownership
  */
-contract SimpleToken is ERC20, Ownable {
+contract SimpleToken is ERC20, Ownable, Pausable {
     
     uint256 public constant MAX_SUPPLY = 10000000 * 10 ** 18;
     
@@ -29,5 +30,30 @@ contract SimpleToken is ERC20, Ownable {
      */
     function burn(uint256 amount) external {
         _burn(msg.sender, amount);
+    }
+    
+    /**
+     * @dev Pause token transfers
+     */
+    function pause() external onlyOwner {
+        _pause();
+    }
+    
+    /**
+     * @dev Unpause token transfers
+     */
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+    
+    /**
+     * @dev Override to add pausable functionality
+     */
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
     }
 }
